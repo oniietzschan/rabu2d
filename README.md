32cd473d07dbd948f603b94f3e587f5bf36f2074
shru <oniietzschan@users.noreply.github.com>
2018-01-12 19:08:57 -0500

    Kyuu.

--- README.md
+++ README.md
@@ -3,6 +3,35 @@ rabu2d
 
 Micro unfriendly love2d libraries and scripts mostly for personal use?
 
+Kyuu
+----
+
+9-patches with support for non-equilateral quads.
+
+```lua
+local Kyuu = require 'kyuu'
+local image
+local widthColLeft  =  8
+local widthColMid   = 16
+local widthColRight =  8
+local heightRowTop =  8
+local heightRowMid = 16
+local heightRowBot = 12
+local patch = Kyuu(
+  love.graphics.newImage('9patch.png'), -- Path to asset is also supported
+  8,  -- left column width
+  16, -- middle column width
+  8,  -- right column width
+  8,  -- top row height
+  16, -- middle row height
+  12, -- bottom row height
+  0, -- image X offset, for sprite atlases and such
+  0, -- image Y offset
+)
+local spriteBatch = patch:getSpriteBatch() -- Literally just a love2d SpriteBatch.
+love.graphics.draw(spriteBatch, 0, 0)
+```
+
 Sebu
 ----
 
---
+++ kyuu.lua
@@ -0,0 +1,172 @@
+local Kyuu = {}
+
+local function assertType(obj, expectedType, name)
+  assert(type(expectedType) == 'string' and type(name) == 'string')
+  if type(obj) ~= expectedType then
+    error(name .. ' must be a ' .. expectedType .. ', got: ' .. tostring(obj), 2)
+  end
+end
+
+local Patch = {}
+local PatchMt = {
+  __index = Patch,
+}
+
+function Kyuu:newPatch(...)
+  return setmetatable({}, PatchMt)
+    :init(...)
+end
+
+function Patch:init(image, colLgt, colMid, colRgt, rowTop, rowMid, rowBot, left, top)
+  top = top or 0
+  left = left or 0
+  return self
+    :setImage(image)
+    :setDimensions(colLgt, colMid, colRgt, rowTop, rowMid, rowBot)
+    :setOffset(left, top)
+end
+
+function Patch:setImage(image)
+  if type(image) == 'string' then
+    image = love.graphics.newImage(image)
+  end
+  self._image = image
+  return self
+end
+
+function Patch:setDimensions(colLgt, colMid, colRgt, rowTop, rowMid, rowBot)
+  assertType(colLgt, 'number', 'column left width')
+  assertType(colMid, 'number', 'column middle width')
+  assertType(colRgt, 'number', 'column right width')
+  assertType(rowTop, 'number', 'row left height')
+  assertType(rowMid, 'number', 'row middle height')
+  assertType(rowBot, 'number', 'row right height')
+  self._colLgt = colLgt
+  self._colMid = colMid
+  self._colRgt = colRgt
+  self._rowTop = rowTop
+  self._rowMid = rowMid
+  self._rowBot = rowBot
+  self._quads = nil
+  return self
+end
+
+function Patch:setOffset(left, top)
+  assertType(top, 'number', 'top')
+  assertType(left, 'number', 'left')
+  self._top = top
+  self._left = left
+  self._quads = nil
+  return self
+end
+
+local TOP_LFT = 'TOP_LFT'
+local TOP_MID = 'TOP_MID'
+local TOP_RGT = 'TOP_RGT'
+local MID_LFT = 'MID_LFT'
+local MID_MID = 'MID_MID'
+local MID_RGT = 'MID_RGT'
+local BOT_LFT = 'BOT_LFT'
+local BOT_MID = 'BOT_MID'
+local BOT_RGT = 'BOT_RGT'
+
+function Patch:getSpriteBatch(w, h)
+  self:_validateSpriteBatchDimensions(w, h)
+  self:_tryGenerateQuads()
+  local batch = love.graphics.newSpriteBatch(self._image, 1000, 'static')
+  local x, y, qw, qh, _ = 0, 0, 0, 0, nil
+  while y < h do
+    local rowName, quadName
+
+    if y == 0 and self._rowTop ~= 0 then
+      rowName = 'TOP_'
+    elseif (h - y) == self._rowBot and self._rowBot ~= 0 then
+      rowName = 'BOT_'
+    else
+      rowName = 'MID_'
+    end
+
+    while x < w do
+      if x == 0 and self._colLft ~= 0 then
+        quadName = rowName .. 'LFT'
+      elseif (w - x) == self._colRgt and self._colRgt ~= 0 then
+        quadName = rowName .. 'RGT'
+      else
+        quadName = rowName .. 'MID'
+      end
+
+      local quad = self._quads[quadName]
+      _, _, qw, qh = quad:getViewport()
+      batch:add(quad, x, y)
+      x = x + qw
+    end
+    x = 0
+    y = y + qh
+  end
+
+  batch:flush()
+  return batch
+end
+
+function Patch:_validateSpriteBatchDimensions(w, h)
+  local xRemainder = (w - self._colLgt - self._colRgt) % self._colMid
+  local yRemainder = (h - self._rowTop - self._rowBot) % self._rowMid
+  if xRemainder ~= 0 or yRemainder ~= 0 then
+    error(('Invalid dimensions: %d, %d Remainder of %d, %d'):format(w, h, xRemainder, yRemainder), 3)
+  end
+end
+
+local mapQuadToComponents = {
+  {TOP_LFT, '_colLgt', '_rowTop'},
+  {TOP_MID, '_colMid', '_rowTop'},
+  {TOP_RGT, '_colRgt', '_rowTop'},
+  {MID_LFT, '_colLgt', '_rowMid'},
+  {MID_MID, '_colMid', '_rowMid'},
+  {MID_RGT, '_colRgt', '_rowMid'},
+  {BOT_LFT, '_colLgt', '_rowBot'},
+  {BOT_MID, '_colMid', '_rowBot'},
+  {BOT_RGT, '_colRgt', '_rowBot'},
+}
+
+function Patch:_tryGenerateQuads()
+  if self._quads then
+    return
+  end
+  self._quads = {}
+  local x, y = 0, 0
+  for i, components in ipairs(mapQuadToComponents) do
+    local quadName = components[1]
+    local w = self[components[2]]
+    local h = self[components[3]]
+    if w > 0 and h > 0 then
+      self._quads[quadName] = love.graphics.newQuad(
+        x + self._left,
+        y + self._top,
+        w,
+        h,
+        self._image:getDimensions()
+      )
+    end
+    if i % 3 ~= 0 then
+      x = x + w
+    else
+      x = 0
+      y = y + h
+    end
+  end
+end
+
+function Patch:drawDebug(x, y)
+  for quadName, quad in pairs(self._quads) do
+    love.graphics.draw(self._image, quad, x, y)
+    love.graphics.print(quadName, x + 20, y)
+    y = y + 21
+  end
+end
+
+local KyuuMt = {
+  __index = Kyuu,
+  __call = Kyuu.newPatch,
+}
+
+return setmetatable({}, KyuuMt)
