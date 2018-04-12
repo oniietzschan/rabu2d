0af11f6434660bb200e60c18f753365a47189a61
shru <oniietzschan@users.noreply.github.com>
2018-04-12 10:44:34 +0930

    Sebu: add IO_MODE_WRITE_ERASE_EXISTING constant.

--- sebu.lua
+++ sebu.lua
@@ -2,13 +2,18 @@ local bitser = require 'lib.bitser'
 
 local Sebu = {}
 
+-- https://www.lua.org/manual/5.1/manual.html#5.7
+-- w+: Update mode, all previous data is erased;
+-- b:  Binary mode, forces Windows to save with Unix endings.
+IO_MODE_WRITE_ERASE_EXISTING = 'w+b'
+
 function Sebu.save(filepath, data)
   assert(type(filepath) == 'string')
   assert(type(data) == 'table')
   local serialized = bitser.dumps(data)
   local file, err = love.filesystem.newFile(filepath)
   assert(err == nil, err)
-  file:open("w")
+  file:open(IO_MODE_WRITE_ERASE_EXISTING)
   file:write(serialized)
   file:close()
 end
